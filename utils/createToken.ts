2655e5df7f9a104af7f19261c790c763714bafb5
FLomka <flomkal@gmail.com>
2025-04-25 16:29:53 +0300

    dissable logger

--- utils/createToken.ts
+++ utils/createToken.ts
@@ -9,9 +9,6 @@ export function createToken(data: object) {
 		})
 	})
 
-	console.log("process.env.TERMINAL_ID", process.env.TERMINAL_ID)
-	console.log("process.env.TERMINAL_PASSWORD", process.env.TERMINAL_PASSWORD)
-
 	sha.update(
 		arr
 			.sort((a, b) => Object.keys(a)[0].localeCompare(Object.keys(b)[0]))
